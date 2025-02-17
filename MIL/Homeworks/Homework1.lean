ab717d0a1a2c5f8d78c76d5cb25dd0f50bf06788
Alvise Sembenico <alvise.sembenico@gmail.com>
2025-02-17 17:18:54 +0100

    add more exercises

--- MIL/Homeworks/Homework1.lean
+++ MIL/Homeworks/Homework1.lean
@@ -14,11 +14,37 @@ Student number: 12380288
 -/
 
 import Mathlib
+variable {R : Type*} [CommRing R]
+
+theorem double(x y : R) : (x+y)*(x+ -y) = x^2 - y^2 := by
+  rw [left_distrib (x+y)]
+  rw [right_distrib, right_distrib]
+  rw [← add_assoc]
+  rw [mul_comm x (-y)]
+  rw [← neg_mul_eq_neg_mul]
+  rw [add_comm]
+  rw [add_assoc]
+  rw [add_neg_cancel (y*x)]
+  rw [add_zero]
+  rw [add_comm]
+  rw [← pow_two]
+  rw [mul_comm y (-y)]
+  rw [← neg_mul_eq_neg_mul]
+  rw [← pow_two]
+  rw [sub_eq_add_neg]
 
 -- (1)
 -- You may NOT use the "ring" tactic in this exercise; use only basic rewrites instead.
-example [CommRing R] (x y a : R) (h : a = x+y+b): (a-b)*(x-y)=x^2-y^2 := by
-  sorry
+-- theorem add_right_neg (a : R) : a + -a = 0 := by rw [add_comm, neg_add_cancel]
+example (x y a b : R) (h : a = x+y+b): (a-b)*(x-y)=x^2-y^2 := by
+  rw [h]
+  rw [sub_eq_add_neg]
+  rw [add_assoc]
+  rw [add_neg_cancel b]
+  rw [add_zero]
+  rw [sub_eq_add_neg]
+  apply double
+
 
 -- (2)
 -- item (a)
